fddd8898d71a1dd37741aba234955af61e23c9c1
Hans <shaomenghao@hotmail.com>
2018-04-14 11:54:50 +0800

    线程基础

---
+++ 1.3.终止线程/Program.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1._3.终止线程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("主线程开始...");
+            //创建一个子线程并启动
+            Thread t = new Thread(Common.PrintNumbersWithDelay);
+            t.Start();
+
+            // 主线程暂停6秒
+            Thread.Sleep(TimeSpan.FromSeconds(6));
+
+            // 终止子线程t
+            t.Abort();
+            Console.WriteLine("子线程终止了...");
+
+            Thread t1 = new Thread(Common.PrintNumbers);
+            t1.Start();
+            Common.PrintNumbers();
+
+            Console.ReadKey();
+
+        }
+    }
+}
