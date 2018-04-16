33a25023353131f89c7795ab9a7fae370264d4be
邵汉中 <635260101@qq.com>
2018-04-16 16:26:18 +0800

    Lock

---
+++ 1.8.Lock关键字/CounterNoLock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._8.Lock关键字
+{
+    public class CounterNoLock : CounterBase
+    {
+        public int Count { get; private set; }
+
+        public override void Decrease()
+        {
+            Count--;
+        }
+
+        public override void Increase()
+        {
+            Count++;
+        }
+    }
+}
---
+++ 1.8.Lock关键字/CounterWithLock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._8.Lock关键字
+{
+    public class CounterWithLock : CounterBase
+    {
+        public int Count { get; private set; }
+
+        private readonly object _syncObj = new object();
+
+        public override void Decrease()
+        {
+            lock(_syncObj)
+            {
+                Count--;
+            }
+        }
+
+        public override void Increase()
+        {
+            lock (_syncObj)
+            {
+                Count++;
+            }
+        }
+    }
+}
