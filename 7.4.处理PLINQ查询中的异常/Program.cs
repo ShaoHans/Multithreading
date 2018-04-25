ac8b3d0a403017e6d5c39bfe2e71b9355c383b04
邵汉中 <635260101@qq.com>
2018-04-25 18:02:30 +0800

    PLINQ

---
+++ 7.4.处理PLINQ查询中的异常/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._4.处理PLINQ查询中的异常
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = Enumerable.Range(-5, 10);
+            var query = from n in numbers select 100 / n;
+            try
+            {
+                foreach (var q in query)
+                {
+                    Console.WriteLine(q);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Linq查询出现异常：{ex.Message}");
+            }
+
+            var parallelQuery = from n in numbers.AsParallel() select 100 / n;
+            try
+            {
+                parallelQuery.ForAll(Console.WriteLine);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"PLinq查询出现异常：{ex.Message}");
+            }
+            catch(AggregateException ex)
+            {
+                ex.Flatten().Handle(e => {
+                    if(e is DivideByZeroException)
+                    {
+                        Console.WriteLine($"捕获到：{ex.Message}");
+                        return true;
+                    }
+                    return false;
+                });
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ 7.5.管理PLINQ查询中的数据分区/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _7._5.管理PLINQ查询中的数据分区
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringPartitioner stringPartitioner = new StringPartitioner(GetProjects());
+            var parallelQuery = from p in stringPartitioner.AsParallel() select ProcessProject(p);
+            parallelQuery.ForAll(PrintProject);
+            Console.ReadKey();
+        }
+
+        static IEnumerable<string> GetProjects()
+        {
+            IList<string> projects = new List<string>();
+            for (int i = 1; i <= 11; i++)
+            {
+                projects.Add($"项目{i}");
+            }
+            return projects;
+        }
+
+        static string ProcessProject(string projectName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(200, 500)));
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}号线程处理{projectName}，" +
+                $"{string.Format(projectName.Length % 2 == 0 ? "偶数" : "奇数")}长度");
+            return projectName;
+        }
+
+        static void PrintProject(string projectName)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(150));
+            Console.WriteLine($"{projectName}在{Thread.CurrentThread.ManagedThreadId}号线程上被输出");
+        }
+    }
+
+    class StringPartitioner: Partitioner<string>
+    {
+        private readonly IEnumerable<string> _data;
+        public override bool SupportsDynamicPartitions => false;
+
+        public StringPartitioner(IEnumerable<string> data)
+        {
+            _data = data;
+        }
+
+        public override IList<IEnumerator<string>> GetPartitions(int partitionCount)
+        {
+            var result = new List<IEnumerator<string>>(2);
+            result.Add(CreateEnumerator(true));
+            result.Add(CreateEnumerator(false));
+            return result;
+        }
+
+        IEnumerator<string> CreateEnumerator(bool isEven)
+        {
+            foreach (var d in _data)
+            {
+                if (!(d.Length % 2 == 0 ^ isEven))
+                {
+                    yield return d;
+                }
+            }
+        }
+    }
+}
