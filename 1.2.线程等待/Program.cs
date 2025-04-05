14e160db24500353e258c9b4e4e8f3accb1523d1
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 21:49:39 +0800

    升级1.2.线程等待到.net9.0

--- 1.2.线程等待/Program.cs
+++ 1.2.线程等待/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Infrastructure;
 
-namespace _1._2.线程等待
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Thread thread = new Thread(Common.PrintNumbersWithDelay);
-            thread.Start();
-            
-            // 等待线程
-            thread.Join();
+namespace _1._2.线程等待;
 
-            Console.WriteLine("打印完成！");
-            Console.ReadKey();
-        }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(Common.PrintNumbersWithDelay);
+        thread.Start();
+
+        // 等待线程
+        thread.Join();
+
+        Console.WriteLine("打印完成！");
+        Console.ReadKey();
     }
 }
