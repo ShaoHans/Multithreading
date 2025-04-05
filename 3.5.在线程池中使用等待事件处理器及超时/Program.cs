460d3ce1515299ac11a82f2cf636dec8bc32e29a
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 22:15:32 +0800

    upgrade 3.5.在线程池中使用等待事件处理器及超时 net9.0

--- 3.5.在线程池中使用等待事件处理器及超时/Program.cs
+++ 3.5.在线程池中使用等待事件处理器及超时/Program.cs
@@ -1,63 +1,64 @@
 ﻿using System;
 using System.Threading;
 
-namespace _3._5.在线程池中使用等待事件处理器及超时
+namespace _3._5.在线程池中使用等待事件处理器及超时;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        RunOperation(TimeSpan.FromSeconds(5));
+        RunOperation(TimeSpan.FromSeconds(7));
+        Console.ReadKey();
+    }
+
+    static void RunOperation(TimeSpan workerOperationTs)
+    {
+        using var evt = new ManualResetEvent(false);
+        using var cts = new CancellationTokenSource();
+        Console.WriteLine("注册操作超时的方法...");
+        var worker = ThreadPool.RegisterWaitForSingleObject(
+            evt,
+            (state, isTimeout) => WorkerOperationWait(cts, isTimeout),
+            null,
+            workerOperationTs,
+            true
+        );
+        Console.WriteLine("开始长时间操作...");
+        ThreadPool.QueueUserWorkItem(_ =>
         {
-            RunOperation(TimeSpan.FromSeconds(5));
-            RunOperation(TimeSpan.FromSeconds(7));
-            Console.ReadKey();
+            WorkerOperation(cts.Token, evt);
+        });
+
+        Thread.Sleep(workerOperationTs.Add(TimeSpan.FromSeconds(2)));
+        worker.Unregister(evt);
+    }
+
+    static void WorkerOperation(CancellationToken token, ManualResetEvent evt)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            if (token.IsCancellationRequested)
+            {
+                Console.WriteLine("异步操作被取消");
+                return;
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(1));
         }
 
-        static void RunOperation(TimeSpan workerOperationTs)
-        {
-            using (var evt = new ManualResetEvent(false))
-            using (var cts = new CancellationTokenSource())
-            {
-                Console.WriteLine("注册操作超时的方法...");
-                var worker = ThreadPool.RegisterWaitForSingleObject(evt,
-                    (state, isTimeout) => WorkerOperationWait(cts, isTimeout),
-                    null, workerOperationTs, true);
-                Console.WriteLine("开始长时间操作...");
-                ThreadPool.QueueUserWorkItem(_ =>
-                {
-                    WorkerOperation(cts.Token, evt);
-                });
+        evt.Set();
+    }
 
-                Thread.Sleep(workerOperationTs.Add(TimeSpan.FromSeconds(2)));
-                worker.Unregister(evt);
-            }
+    static void WorkerOperationWait(CancellationTokenSource cts, bool isTimeout)
+    {
+        if (isTimeout)
+        {
+            cts.Cancel();
+            Console.WriteLine("异步操作超时并被取消了");
         }
-
-        static void WorkerOperation(CancellationToken token, ManualResetEvent evt)
+        else
         {
-            for (int i = 0; i < 6; i++)
-            {
-                if (token.IsCancellationRequested)
-                {
-                    Console.WriteLine("异步操作被取消");
-                    return;
-                }
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-            }
-
-            evt.Set();
-        }
-
-        static void WorkerOperationWait(CancellationTokenSource cts, bool isTimeout)
-        {
-            if (isTimeout)
-            {
-                cts.Cancel();
-                Console.WriteLine("异步操作超时并被取消了");
-            }
-            else
-            {
-                Console.WriteLine("异步操作完成");
-            }
+            Console.WriteLine("异步操作完成");
         }
     }
 }
--- 3.5.在线程池中使用等待事件处理器及超时/Properties/AssemblyInfo.cs
+++ 3.5.在线程池中使用等待事件处理器及超时/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("3.5.在线程池中使用等待事件处理器及超时")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("3.5.在线程池中使用等待事件处理器及超时")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("b384cd80-bb3c-4fef-a3d9-1d66a1e8d63e")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
