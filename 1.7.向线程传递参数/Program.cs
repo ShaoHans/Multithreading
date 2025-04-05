74285e984611400e1b367e41b166961d64c26825
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 22:03:18 +0800

    upgrade 1.7.向线程传递参数 net9.0

--- 1.7.向线程传递参数/Program.cs
+++ 1.7.向线程传递参数/Program.cs
@@ -1,85 +1,89 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace _1._7.向线程传递参数
+namespace _1._7.向线程传递参数;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var sample = new ThreadSample(10);
+        var t1 = new Thread(sample.CountNumbers);
+        t1.Name = "t1";
+        t1.Start();
+        t1.Join();
+        Console.WriteLine("===================================");
+
+        var t2 = new Thread(Count);
+        t2.Name = "t2";
+        t2.Start(8);
+        t2.Join();
+        Console.WriteLine("===================================");
+
+        // 使用lambda表达式引用另一个C#对象的方式被称为闭包
+        var t3 = new Thread(() =>
         {
-            var sample = new ThreadSample(10);
-            var t1 = new Thread(sample.CountNumbers);
-            t1.Name = "t1";
-            t1.Start();
-            t1.Join();
-            Console.WriteLine("===================================");
+            CountNumbers(9);
+        });
+        t3.Name = "t3";
+        t3.Start();
+        t3.Join();
+        Console.WriteLine("===================================");
 
-            var t2 = new Thread(Count);
-            t2.Name = "t2";
-            t2.Start(8);
-            t2.Join();
-            Console.WriteLine("===================================");
-
-            // 使用lambda表达式引用另一个C#对象的方式被称为闭包
-            var t3 = new Thread(() => { CountNumbers(9); });
-            t3.Name = "t3";
-            t3.Start();
-            t3.Join();
-            Console.WriteLine("===================================");
-
-            // 在lambda表达式中使用任何局部变量时，C#会生成一个类，并将该变量作为该类的一个属性
-            // 共享该变量值
-            int i = 10;
-            var t4 = new Thread(() => { PrintNumbers(i); });
-            t4.Name = "t4";
-            i = 12;
-            var t5 = new Thread(() => { PrintNumbers(i); });
-            t5.Name = "t4";
-            t4.Start();
-            t5.Start();
-            Console.ReadKey();
-        }
-
-        static void CountNumbers(int count)
+        // 在lambda表达式中使用任何局部变量时，C#会生成一个类，并将该变量作为该类的一个属性
+        // 共享该变量值
+        int i = 10;
+        var t4 = new Thread(() =>
         {
-            for (int i = 0; i < count; i++)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(0.5));
-                Console.WriteLine($"线程{Thread.CurrentThread.Name}输出{i}");
-            }
-        }
-
-        static void Count(object obj)
+            PrintNumbers(i);
+        });
+        t4.Name = "t4";
+        i = 12;
+        var t5 = new Thread(() =>
         {
-            CountNumbers((int)obj);
-        }
+            PrintNumbers(i);
+        });
+        t5.Name = "t4";
+        t4.Start();
+        t5.Start();
+        Console.ReadKey();
+    }
 
-        static void PrintNumbers(int number)
+    static void CountNumbers(int count)
+    {
+        for (int i = 0; i < count; i++)
         {
-            Console.WriteLine($"线程{Thread.CurrentThread.Name}输出{number}");
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            Console.WriteLine($"线程{Thread.CurrentThread.Name}输出{i}");
         }
     }
 
-    class ThreadSample
+    static void Count(object obj)
     {
-        private readonly int _count;
+        CountNumbers((int)obj);
+    }
 
-        public ThreadSample(int count)
-        {
-            _count = count;
-        }
+    static void PrintNumbers(int number)
+    {
+        Console.WriteLine($"线程{Thread.CurrentThread.Name}输出{number}");
+    }
+}
 
-        public void CountNumbers()
+class ThreadSample
+{
+    private readonly int _count;
+
+    public ThreadSample(int count)
+    {
+        _count = count;
+    }
+
+    public void CountNumbers()
+    {
+        for (int i = 0; i < _count; i++)
         {
-            for (int i = 0; i < _count; i++)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(0.5));
-                Console.WriteLine($"{Thread.CurrentThread.Name} 输出 {i}");
-            }
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            Console.WriteLine($"{Thread.CurrentThread.Name} 输出 {i}");
         }
     }
 }
