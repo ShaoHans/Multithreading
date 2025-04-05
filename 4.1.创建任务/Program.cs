80ead3dbbb24783904ba0bf359dcafb2e519562d
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 22:17:09 +0800

    upgrade 4.1.创建任务 net9.0

--- 4.1.创建任务/Program.cs
+++ 4.1.创建任务/Program.cs
@@ -2,30 +2,48 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace _4._1.创建任务
+namespace _4._1.创建任务;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var t1 = new Task(() =>
         {
-            var t1 = new Task(() => { TaskMethod("task1"); });
-            var t2 = new Task(() => { TaskMethod("task2"); });
-            t1.Start();
-            t2.Start();
-
-            Task.Run(() => { TaskMethod("task3"); });
-            Task.Factory.StartNew(() => { TaskMethod("task4"); });
-            Task.Factory.StartNew(() => { TaskMethod("task5"); }, TaskCreationOptions.LongRunning);
-
-
-            Console.ReadKey();
-        }
-
-        static void TaskMethod(string taskName)
+            TaskMethod("task1");
+        });
+        var t2 = new Task(() =>
         {
-            Console.WriteLine($"任务 {taskName} 正在运行，" +
-                $"线程id： {Thread.CurrentThread.ManagedThreadId}" +
-                $"，是否为线程池中的线程：{Thread.CurrentThread.IsThreadPoolThread}");
-        }
+            TaskMethod("task2");
+        });
+        t1.Start();
+        t2.Start();
+
+        Task.Run(() =>
+        {
+            TaskMethod("task3");
+        });
+        Task.Factory.StartNew(() =>
+        {
+            TaskMethod("task4");
+        });
+        Task.Factory.StartNew(
+            () =>
+            {
+                TaskMethod("task5");
+            },
+            TaskCreationOptions.LongRunning
+        );
+
+        Console.ReadKey();
+    }
+
+    static void TaskMethod(string taskName)
+    {
+        Console.WriteLine(
+            $"任务 {taskName} 正在运行，"
+                + $"线程id： {Thread.CurrentThread.ManagedThreadId}"
+                + $"，是否为线程池中的线程：{Thread.CurrentThread.IsThreadPoolThread}"
+        );
     }
 }
