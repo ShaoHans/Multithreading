09c07fbed365e03852a202b842eb2584d4f158b4
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 22:35:31 +0800

    upgrade 5.4.对并行执行的异步任务使用await操作符 net9.0

--- Infrastructure/Common.cs
+++ Infrastructure/Common.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Threading;
 
-namespace Infrastructure
+namespace Infrastructure;
+
+public static class Common
 {
-    public static class Common
+    public static void PrintNumbers()
     {
-        public static void PrintNumbers()
+        Console.WriteLine("开始打印数字...");
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("开始打印数字...");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"当前线程Id：{Thread.CurrentThread.ManagedThreadId}，数字：{i}");
-            }
+            Console.WriteLine($"当前线程Id：{Environment.CurrentManagedThreadId}，数字：{i}");
         }
+    }
 
-        public static void PrintNumbersWithDelay()
+    public static void PrintNumbersWithDelay()
+    {
+        Console.WriteLine("开始打印数字...");
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("开始打印数字...");
-            for (int i = 0; i < 10; i++)
-            {
-                // 暂停线程
-                Thread.Sleep(1000);
-                Console.WriteLine($"当前线程Id：{Thread.CurrentThread.ManagedThreadId}，数字：{i}");
-            }
+            // 暂停线程
+            Thread.Sleep(1000);
+            Console.WriteLine($"当前线程Id：{Environment.CurrentManagedThreadId}，数字：{i}");
         }
+    }
 
-        public static void PrintNumbersWithStatus()
+    public static void PrintNumbersWithStatus()
+    {
+        Console.WriteLine("开始打印数字...");
+        Console.WriteLine(
+            $"当前线程{Thread.CurrentThread.Name}的状态：{Thread.CurrentThread.ThreadState}"
+        );
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("开始打印数字...");
-            Console.WriteLine($"当前线程{Thread.CurrentThread.Name}的状态：{Thread.CurrentThread.ThreadState.ToString()}");
-            for (int i = 0; i < 10; i++)
-            {
-                // 暂停线程
-                Thread.Sleep(1000);
-                Console.WriteLine($"当前线程Id：{Thread.CurrentThread.ManagedThreadId}，数字：{i}");
-            }
+            // 暂停线程
+            Thread.Sleep(1000);
+            Console.WriteLine($"当前线程Id：{Environment.CurrentManagedThreadId}，数字：{i}");
         }
     }
 }
