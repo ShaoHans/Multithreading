8eee448ee74668bfcb02e61a085ce766d1119679
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 22:06:24 +0800

    upgrade 1.9.使用Monitor类锁定资源 net9.0

--- 1.9.使用Monitor类锁定资源/Player.cs
+++ 1.9.使用Monitor类锁定资源/Player.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _1._9.使用Monitor类锁定资源
+namespace _1._9.使用Monitor类锁定资源;
+
+/// <summary>
+/// 玩家
+/// </summary>
+public class Player
 {
-    /// <summary>
-    /// 玩家
-    /// </summary>
-    public class Player
+    public string Name { get; private set; }
+
+    public int Atk { get; private set; }
+
+    public Player(string name, int atk)
     {
-        public string Name { get; private set; }
+        Name = name;
+        Atk = atk;
+    }
 
-        public int Atk { get; private set; }
-
-        public Player(string name, int atk)
+    public void Attack(YaoGuai yaoGuai)
+    {
+        while (yaoGuai.Blood > 0)
         {
-            Name = name;
-            Atk = atk;
-        }
-
-        public void Attack(YaoGuai yaoGuai)
-        {
-            while(yaoGuai.Blood > 0)
-            {
-                Console.WriteLine($"我是{Name}，我来打妖怪~");
-                yaoGuai.BeAttacked(Atk);
-            }
+            Console.WriteLine($"我是{Name}，我来打妖怪~");
+            yaoGuai.BeAttacked(Atk);
         }
     }
 }
--- 1.9.使用Monitor类锁定资源/YaoGuai.cs
+++ 1.9.使用Monitor类锁定资源/YaoGuai.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _1._9.使用Monitor类锁定资源
+namespace _1._9.使用Monitor类锁定资源;
+
+/// <summary>
+/// 妖怪
+/// </summary>
+public class YaoGuai
 {
-    /// <summary>
-    /// 妖怪
-    /// </summary>
-    public class YaoGuai
+    public int Blood { get; private set; }
+
+    public YaoGuai(int blood)
     {
-        public int Blood { get; private set; }
+        Blood = blood;
+        Console.WriteLine($"我是妖怪，我有{Blood}滴血！");
+    }
 
-        public YaoGuai(int blood)
+    public void BeAttacked(int attack)
+    {
+        if (Blood > 0)
         {
-            Blood = blood;
-            Console.WriteLine($"我是妖怪，我有{Blood}滴血！");
-        }
-
-        public void BeAttacked(int attack)
-        {
-            if (Blood > 0)
+            Blood = Blood - attack;
+            if (Blood < 0)
             {
-                Blood = Blood - attack;
-                if (Blood < 0)
-                {
-                    Blood = 0;
-                }
+                Blood = 0;
             }
-            Console.WriteLine($"我是妖怪，我剩余{Blood}滴血！");
         }
+        Console.WriteLine($"我是妖怪，我剩余{Blood}滴血！");
     }
 }
