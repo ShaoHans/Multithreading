04c801582574ce7653a2fa1a284f2bb59f9a05a7
ShaoHans <shaomenghao@hotmail.com>
2025-04-05 21:59:58 +0800

    upgrade 1.5.线程优先级 net9.0

--- 1.4.检测线程状态/Program.cs
+++ 1.4.检测线程状态/Program.cs
@@ -1,45 +1,38 @@
-﻿using Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading;
-using System.Threading.Tasks;
+using Infrastructure;
 
-namespace _1._4.检测线程状态
+namespace _1._4.检测线程状态;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Thread t1 = new Thread(Common.PrintNumbersWithStatus);
+        t1.Name = "t1";
+        Thread t2 = new Thread(DoNothing);
+        t2.Name = "t2";
+        Console.WriteLine($"线程{t1.Name}的状态是：{t1.ThreadState.ToString()}");
+        Console.WriteLine($"线程{t2.Name}的状态是：{t2.ThreadState.ToString()}");
+
+        t1.Start();
+        t2.Start();
+
+        for (int i = 0; i < 30; i++)
         {
-            Thread t1 = new Thread(Common.PrintNumbersWithStatus);
-            t1.Name = "t1";
-            Thread t2 = new Thread(DoNothing);
-            t2.Name = "t2";
             Console.WriteLine($"线程{t1.Name}的状态是：{t1.ThreadState.ToString()}");
-            Console.WriteLine($"线程{t2.Name}的状态是：{t2.ThreadState.ToString()}");
-
-            t1.Start();
-            t2.Start();
-
-            for (int i = 0; i < 30; i++)
-            {
-                Console.WriteLine($"线程{t1.Name}的状态是：{t1.ThreadState.ToString()}");
-            }
-
-            Thread.Sleep(TimeSpan.FromSeconds(4));
-            t1.Abort();
-            Console.WriteLine($"线程{t1.Name}的状态是：{t1.ThreadState.ToString()}");
-            Console.WriteLine($"线程{t2.Name}的状态是：{t2.ThreadState.ToString()}");
-
-            Console.ReadKey();
         }
 
-        static void DoNothing()
-        {
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-        }
+        Thread.Sleep(TimeSpan.FromSeconds(4));
+        t1.Abort();
+        Console.WriteLine($"线程{t1.Name}的状态是：{t1.ThreadState.ToString()}");
+        Console.WriteLine($"线程{t2.Name}的状态是：{t2.ThreadState.ToString()}");
 
-        
+        Console.ReadKey();
+    }
+
+    static void DoNothing()
+    {
+        Thread.Sleep(TimeSpan.FromSeconds(2));
     }
 }
--- 1.5.线程优先级/Program.cs
+++ 1.5.线程优先级/Program.cs
@@ -1,64 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace _1._5.线程优先级
+namespace _1._5.线程优先级;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine($"主线程{Thread.CurrentThread.Name}的优先级是{Thread.CurrentThread.Priority}");
-            Console.WriteLine("在所有CPU内核上运行线程");
-            RunThreads();
-            Thread.Sleep(TimeSpan.FromSeconds(3));
-            Console.WriteLine("在一个CPU内核上运行线程");
-            Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
-            RunThreads();
-            Console.ReadKey();
-        }
-
-        static void RunThreads()
-        {
-            ThreadSample sample = new ThreadSample();
-            Thread t1 = new Thread(sample.CountNumbers);
-            t1.Name = "t1";
-            Thread t2 = new Thread(sample.CountNumbers);
-            t2.Name = "t2";
-
-            t1.Priority = ThreadPriority.Highest;
-            t2.Priority = ThreadPriority.Lowest;
-
-            t1.Start();
-            t2.Start();
-
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            sample.Stop();
-        }
+        Console.WriteLine($"主线程{Thread.CurrentThread.Name}的优先级是{Thread.CurrentThread.Priority}");
+        Console.WriteLine("在所有CPU内核上运行线程");
+        RunThreads();
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        Console.WriteLine("在一个CPU内核上运行线程");
+        Process.GetCurrentProcess().ProcessorAffinity = new IntPtr(1);
+        RunThreads();
+        Console.ReadKey();
     }
 
-    class ThreadSample
+    static void RunThreads()
     {
-        private bool _isStopped = false;
+        ThreadSample sample = new ThreadSample();
+        Thread t1 = new Thread(sample.CountNumbers);
+        t1.Name = "t1";
+        Thread t2 = new Thread(sample.CountNumbers);
+        t2.Name = "t2";
 
-        public void Stop()
+        t1.Priority = ThreadPriority.Highest;
+        t2.Priority = ThreadPriority.Lowest;
+
+        t1.Start();
+        t2.Start();
+
+        Thread.Sleep(TimeSpan.FromSeconds(2));
+        sample.Stop();
+    }
+}
+
+class ThreadSample
+{
+    private bool _isStopped = false;
+
+    public void Stop()
+    {
+        _isStopped = true;
+    }
+
+    public void CountNumbers()
+    {
+        long count = 0;
+        while (!_isStopped)
         {
-            _isStopped = true;
+            count++;
         }
 
-        public void CountNumbers()
-        {
-            long count = 0;
-            while(!_isStopped)
-            {
-                count++;
-            }
-
-            Console.WriteLine($"线程{Thread.CurrentThread.Name}的优先级是{Thread.CurrentThread.Priority},计数器的值为{count}");
-        }
+        Console.WriteLine(
+            $"线程{Thread.CurrentThread.Name}的优先级是{Thread.CurrentThread.Priority},计数器的值为{count}"
+        );
     }
 }
