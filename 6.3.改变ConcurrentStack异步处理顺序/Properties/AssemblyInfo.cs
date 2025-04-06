17c4347b1fe86cbcf003ee760999c041fc1ff078
ShaoHans <shaomenghao@hotmail.com>
2025-04-06 09:36:03 +0800

    upgrade 6.3.改变ConcurrentStack异步处理顺序 net9.0

--- 6.3.改变ConcurrentStack异步处理顺序/Properties/AssemblyInfo.cs
+++ 6.3.改变ConcurrentStack异步处理顺序/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("6.3.改变ConcurrentStack异步处理顺序")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("6.3.改变ConcurrentStack异步处理顺序")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("10c6ff1e-54f0-4778-a83b-f5d2db24a6b6")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
