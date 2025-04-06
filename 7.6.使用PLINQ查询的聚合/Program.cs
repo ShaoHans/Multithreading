fa378c95cf04e9a9edbeb0c13f1a1a9fe5e6703d
ShaoHans <shaomenghao@hotmail.com>
2025-04-06 10:14:08 +0800

    upgrade 7.6.使用PLINQ查询的聚合 net9.0

--- 7.6.使用PLINQ查询的聚合/Program.cs
+++ 7.6.使用PLINQ查询的聚合/Program.cs
@@ -3,56 +3,56 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace _7._6.使用PLINQ查询的聚合
+namespace _7._6.使用PLINQ查询的聚合;
+
+class Program
 {
-    class Program
+    static int NUM_INTS = 500000000;
+
+    static IEnumerable<int> GenerateInputeData()
     {
-        static int NUM_INTS = 500000000;
+        return Enumerable.Range(1, NUM_INTS);
+    }
 
-        static IEnumerable<int> GenerateInputeData()
-        {
-            return Enumerable.Range(1, NUM_INTS);
-        }
+    static ParallelQuery<int> GenerateInputeData4Parallel()
+    {
+        return ParallelEnumerable.Range(1, NUM_INTS);
+    }
 
-        static ParallelQuery<int> GenerateInputeData4Parallel()
-        {
-            return ParallelEnumerable.Range(1, NUM_INTS);
-        }
+    static void Main(string[] args)
+    {
+        var seqTarget = GenerateInputeData();
 
-        static void Main(string[] args)
-        {
-            var seqTarget = GenerateInputeData();
+        Console.WriteLine("============================================================");
+        Console.WriteLine("TEST NORMAL LINQ");
+        Console.WriteLine("============================================================");
+        var swatchpn = Stopwatch.StartNew();
 
-            Console.WriteLine("============================================================");
-            Console.WriteLine("TEST NORMAL LINQ");
-            Console.WriteLine("============================================================");
-            var swatchpn = Stopwatch.StartNew();
+        var seqQuery = (
+            from intNum in seqTarget
+            where ((intNum % 5) == 0)
+            select (intNum / Math.PI)
+        ).Average();
+        swatchpn.Stop();
 
-            var seqQuery = (from intNum in seqTarget
-                            where ((intNum % 5) == 0)
-                            select (intNum / Math.PI)).Average();
-            swatchpn.Stop();
+        Console.WriteLine("LINQ Result: " + seqQuery + "    LINQ Use Time: {0}", swatchpn.Elapsed);
 
-            Console.WriteLine("LINQ Result: " + seqQuery + "    LINQ Use Time: {0}", swatchpn.Elapsed);
+        var palTarget = GenerateInputeData4Parallel();
+        Console.WriteLine("\n\n");
+        Console.WriteLine("============================================================");
+        Console.WriteLine("TEST PARALLEL LINQ");
+        Console.WriteLine("============================================================");
+        var swatchp = Stopwatch.StartNew();
 
+        var palQuery = (
+            from intNum in palTarget.AsParallel()
+            where ((intNum % 5) == 0)
+            select (intNum / Math.PI)
+        ).Average();
+        swatchp.Stop();
 
+        Console.WriteLine("PLINQ Result: " + palQuery + "    LINQ Use Time: {0}", swatchp.Elapsed);
 
-            var palTarget = GenerateInputeData4Parallel();
-            Console.WriteLine("\n\n");
-            Console.WriteLine("============================================================");
-            Console.WriteLine("TEST PARALLEL LINQ");
-            Console.WriteLine("============================================================");
-            var swatchp = Stopwatch.StartNew();
-
-            var palQuery = (from intNum in palTarget.AsParallel()
-                            where ((intNum % 5) == 0)
-                            select (intNum / Math.PI)).Average();
-            swatchp.Stop();
-
-            Console.WriteLine("PLINQ Result: " + palQuery + "    LINQ Use Time: {0}", swatchp.Elapsed);
-
-            Console.ReadLine();
-
-        }
+        Console.ReadLine();
     }
 }
