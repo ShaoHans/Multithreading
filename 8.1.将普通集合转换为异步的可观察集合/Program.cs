bec39ebfdd7c736989d3ef198d188eceb5030059
ShaoHans <shaomenghao@hotmail.com>
2025-04-06 10:16:59 +0800

    upgrade 8.1.将普通集合转换为异步的可观察集合 net9.0

--- 8.1.将普通集合转换为异步的可观察集合/Program.cs
+++ 8.1.将普通集合转换为异步的可观察集合/Program.cs
@@ -4,41 +4,41 @@ using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 
-namespace _8._1.将普通集合转换为异步的可观察集合
+namespace _8._1.将普通集合转换为异步的可观察集合;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        foreach (var i in GenerateIntList())
         {
-            foreach (var i in GenerateIntList())
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("IEnumerable");
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("IEnumerable");
 
-
-            IObservable<int> o = GenerateIntList().ToObservable();
-            using (IDisposable sub = o.Subscribe(Console.WriteLine))
-            {
-                Console.WriteLine("IObservable");
-            }
-
-            IObservable<int> o2 = GenerateIntList().ToObservable().SubscribeOn(TaskPoolScheduler.Default);
-            using (IDisposable sub = o2.Subscribe(Console.WriteLine))
-            {
-                //Thread.Sleep(1000);
-                Console.WriteLine("IObservable async");
-            }
-            Console.ReadKey();
+        IObservable<int> o = GenerateIntList().ToObservable();
+        using (IDisposable sub = o.Subscribe(Console.WriteLine))
+        {
+            Console.WriteLine("IObservable");
         }
 
-        static IEnumerable<int> GenerateIntList()
+        IObservable<int> o2 = GenerateIntList()
+            .ToObservable()
+            .SubscribeOn(TaskPoolScheduler.Default);
+        using (IDisposable sub = o2.Subscribe(Console.WriteLine))
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Thread.Sleep(TimeSpan.FromSeconds(0.5));
-                yield return i;
-            }
+            //Thread.Sleep(1000);
+            Console.WriteLine("IObservable async");
+        }
+        Console.ReadKey();
+    }
+
+    static IEnumerable<int> GenerateIntList()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            yield return i;
         }
     }
 }
