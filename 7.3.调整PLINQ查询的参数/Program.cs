d83625d8e5f2b65ac2164f880cc8ef53de9d0224
ShaoHans <shaomenghao@hotmail.com>
2025-04-06 09:44:31 +0800

    upgrade 7.3.调整PLINQ查询的参数 net9.0

--- 7.3.调整PLINQ查询的参数/Program.cs
+++ 7.3.调整PLINQ查询的参数/Program.cs
@@ -3,65 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-namespace _7._3.调整PLINQ查询的参数
+namespace _7._3.调整PLINQ查询的参数;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var parallelQuery = from p in GetProjects().AsParallel() select ProcessProject(p);
+        var cts = new CancellationTokenSource();
+        cts.CancelAfter(TimeSpan.FromSeconds(1));
+
+        try
         {
-            var parallelQuery = from p in GetProjects().AsParallel() select ProcessProject(p);
-            var cts = new CancellationTokenSource();
-            cts.CancelAfter(TimeSpan.FromSeconds(1));
-
-            try
-            {
-                parallelQuery.WithDegreeOfParallelism(Environment.ProcessorCount)
-                    .WithExecutionMode(ParallelExecutionMode.Default)
-                    .WithMergeOptions(ParallelMergeOptions.Default)
-                    .WithCancellation(cts.Token)
-                    .ForAll(PrintProject);
-            }
-            catch (OperationCanceledException)
-            {
-                Console.WriteLine("并行查询被取消");
-            }
-
-            Console.WriteLine("无序并行查询");
-            var unOrderedQuery = from i in ParallelEnumerable.Range(1, 30) select i;
-            foreach (var i in unOrderedQuery)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.WriteLine("有序并行查询");
-            var orderedQuery = from i in ParallelEnumerable.Range(1, 30).AsOrdered() select i;
-            foreach (var i in orderedQuery)
-            {
-                Console.WriteLine(i);
-            }
-
-            Console.ReadKey();
+            parallelQuery
+                .WithDegreeOfParallelism(Environment.ProcessorCount)
+                .WithExecutionMode(ParallelExecutionMode.Default)
+                .WithMergeOptions(ParallelMergeOptions.Default)
+                .WithCancellation(cts.Token)
+                .ForAll(PrintProject);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("并行查询被取消");
         }
 
-        static IEnumerable<string> GetProjects()
+        Console.WriteLine("无序并行查询");
+        var unOrderedQuery = from i in ParallelEnumerable.Range(1, 30) select i;
+        foreach (var i in unOrderedQuery)
         {
-            for (int i = 1; i <= 20; i++)
-            {
-                yield return $"项目{i}";
-            }
+            Console.WriteLine(i);
         }
 
-        static string ProcessProject(string projectName)
+        Console.WriteLine("有序并行查询");
+        var orderedQuery = from i in ParallelEnumerable.Range(1, 30).AsOrdered() select i;
+        foreach (var i in orderedQuery)
         {
-            Thread.Sleep(TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(200, 500)));
-            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}号线程处理{projectName}");
-            return projectName;
+            Console.WriteLine(i);
         }
 
-        static void PrintProject(string projectName)
+        Console.ReadKey();
+    }
+
+    static IEnumerable<string> GetProjects()
+    {
+        for (int i = 1; i <= 20; i++)
         {
-            Thread.Sleep(TimeSpan.FromMilliseconds(150));
-            Console.WriteLine($"{projectName}在{Thread.CurrentThread.ManagedThreadId}号线程上被输出");
+            yield return $"项目{i}";
         }
+    }
+
+    static string ProcessProject(string projectName)
+    {
+        Thread.Sleep(
+            TimeSpan.FromMilliseconds(new Random(DateTime.Now.Millisecond).Next(200, 500))
+        );
+        Console.WriteLine($"{Environment.CurrentManagedThreadId}号线程处理{projectName}");
+        return projectName;
+    }
+
+    static void PrintProject(string projectName)
+    {
+        Thread.Sleep(TimeSpan.FromMilliseconds(150));
+        Console.WriteLine($"{projectName}在{Environment.CurrentManagedThreadId}号线程上被输出");
     }
 }
