2c60cc401e86e15db1ef0b58454a51aafa604a74
ShaoHans <shaomenghao@hotmail.com>
2025-04-06 09:33:41 +0800

    upgrade 6.1.使用ConcurrentDictionary net9.0

--- 6.1.使用ConcurrentDictionary/Program.cs
+++ 6.1.使用ConcurrentDictionary/Program.cs
@@ -3,61 +3,58 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace _6._1.使用ConcurrentDictionary
+namespace _6._1.使用ConcurrentDictionary;
+
+class Program
 {
-    class Program
+    const string Item = "Dictionary Item";
+    public static string CurrentItem;
+
+    static void Main(string[] args)
     {
-        const string Item = "Dictionary Item";
-        public static string CurrentItem;
+        // ConcurrentDictionary的实现使用了细粒度锁技术
+        ConcurrentDictionary<int, string> conDict = new ConcurrentDictionary<int, string>();
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+        Stopwatch sw = new Stopwatch();
 
-        static void Main(string[] args)
+        sw.Start();
+        for (int i = 0; i < 1000000; i++)
         {
-            // ConcurrentDictionary的实现使用了细粒度锁技术
-            ConcurrentDictionary<int, string> conDict = new ConcurrentDictionary<int, string>();
-            Dictionary<int, string> dict = new Dictionary<int, string>();
-            Stopwatch sw = new Stopwatch();
-
-            sw.Start();
-            for (int i = 0; i < 1000000; i++)
+            lock (dict)
             {
-                lock (dict)
-                {
-                    dict[i] = Item;
-                }
+                dict[i] = Item;
             }
-            sw.Stop();
-            Console.WriteLine($"往加锁的字典对象里面写入值花费时间：{sw.Elapsed}");
-
-            sw.Restart();
-            for (int i = 0; i < 1000000; i++)
-            {
-                conDict[i] = Item;
-            }
-            sw.Stop();
-            Console.WriteLine($"往并发类型的字典对象里面写入值花费时间：{sw.Elapsed}");
-
-            sw.Restart();
-            for (int i = 0; i < 1000000; i++)
-            {
-                lock (dict)
-                {
-                    CurrentItem = dict[i];
-                }
-            }
-            sw.Stop();
-            Console.WriteLine($"从加锁的字典对象里面读取值花费时间：{sw.Elapsed}");
-
-            sw.Restart();
-            for (int i = 0; i < 1000000; i++)
-            {
-                CurrentItem = conDict[i];
-            }
-            sw.Stop();
-            Console.WriteLine($"从并发类型的字典对象里面读取值花费时间：{sw.Elapsed}");
-
-            Console.ReadKey();
         }
+        sw.Stop();
+        Console.WriteLine($"往加锁的字典对象里面写入值花费时间：{sw.Elapsed}");
 
+        sw.Restart();
+        for (int i = 0; i < 1000000; i++)
+        {
+            conDict[i] = Item;
+        }
+        sw.Stop();
+        Console.WriteLine($"往并发类型的字典对象里面写入值花费时间：{sw.Elapsed}");
 
+        sw.Restart();
+        for (int i = 0; i < 1000000; i++)
+        {
+            lock (dict)
+            {
+                CurrentItem = dict[i];
+            }
+        }
+        sw.Stop();
+        Console.WriteLine($"从加锁的字典对象里面读取值花费时间：{sw.Elapsed}");
+
+        sw.Restart();
+        for (int i = 0; i < 1000000; i++)
+        {
+            CurrentItem = conDict[i];
+        }
+        sw.Stop();
+        Console.WriteLine($"从并发类型的字典对象里面读取值花费时间：{sw.Elapsed}");
+
+        Console.ReadKey();
     }
 }
--- 6.1.使用ConcurrentDictionary/Properties/AssemblyInfo.cs
+++ 6.1.使用ConcurrentDictionary/Properties/AssemblyInfo.cs
@@ -1,15 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("6.1.使用ConcurrentDictionary")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("6.1.使用ConcurrentDictionary")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +10,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("a1ec902b-0ff6-44c4-8ebc-31e798d9b9cf")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-// 方法是按如下所示使用“*”: :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
